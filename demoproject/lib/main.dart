f2cb88f2b3b7edcc2c45a5074a058f1ba7e514ad
halinchanok <116154769+halinchanok@users.noreply.github.com>
2024-12-21 17:29:04 +0700

    2

--- demoproject/lib/main.dart
+++ demoproject/lib/main.dart
@@ -18,30 +18,116 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 255, 234, 158),
-        appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 253, 236),
+        appBar: AppBar( 
           title: const Text(
             'Flutter Demo', 
             style: TextStyle(color: Color.fromARGB(255, 2, 108, 28)),
           ),
-          backgroundColor: const Color.fromARGB(255, 145, 255, 117),
+          backgroundColor: const Color.fromARGB(255, 255, 207, 207),
         ),
 
-        body: Container(
-            height: 100,
-            width: 100,
-            color: const Color.fromARGB(255, 255, 178, 63),
-            padding: const EdgeInsets.all(20),
-            margin: const EdgeInsets.all(20),
-            child: const Text("Hello")
-        ),
+        body: 
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                print("Pressed !");
+              }, child: const Text("Press Me"))
+
+          ),
+
+//         Container(
+//           height: 200,
+//           color: Colors.pink[100],
+          
+// // Row
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround, //เเกน x 
+//             crossAxisAlignment: CrossAxisAlignment.center, //แกน y
+//             children: [ 
+//               Container(
+//                 height: 100,
+//                 width: 100,
+//                 decoration: BoxDecoration(
+//                   color: const Color.fromARGB(255, 255, 226, 226),
+//                   borderRadius: BorderRadius.circular(20), // ขอบมน
+//                 ),
+//                 padding: const EdgeInsets.all(20),
+//                 child: const Text("Item 1",
+//                   style: TextStyle(color: Colors.black),), 
+//               ),
+//               Container(
+//                 height: 100,
+//                 width: 100,
+//                 decoration: BoxDecoration(
+//                   color: const Color.fromARGB(255, 255, 226, 226),
+//                   borderRadius: BorderRadius.circular(20), // ขอบมน
+//                 ),
+//                 padding: const EdgeInsets.all(20),
+//                 child: const Text("Item 2",
+//                   style: TextStyle(color: Colors.black),), 
+//               ),
+//               Container(
+//                 height: 100,
+//                 width: 100,
+//                 decoration: BoxDecoration(
+//                   color: const Color.fromARGB(255, 255, 226, 226),
+//                   borderRadius: BorderRadius.circular(20), // ขอบมน
+//                 ),
+//                 padding: const EdgeInsets.all(20),
+//                 child: const Text("Item 3",
+//                   style: TextStyle(color: Colors.black),), 
+//               ),
+//           ]),
+//         ),
+
+// Column
+        // child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.spaceAround, //เเกน x 
+        //     crossAxisAlignment: CrossAxisAlignment.end, //แกน y
+        //     children: [ 
+        //       Container(
+        //         height: 100,
+        //         width: 100,
+        //         decoration: BoxDecoration(
+        //           color: const Color.fromARGB(255, 255, 178, 63),
+        //           borderRadius: BorderRadius.circular(20), // ขอบมน
+        //         ),
+        //         padding: const EdgeInsets.all(20),
+        //         child: const Text("Item 1",
+        //           style: TextStyle(color: Colors.black),), 
+        //       ),
+        //       Container(
+        //         height: 100,
+        //         width: 100,
+        //         decoration: BoxDecoration(
+        //           color: const Color.fromARGB(255, 255, 178, 63),
+        //           borderRadius: BorderRadius.circular(20), // ขอบมน
+        //         ),
+        //         padding: const EdgeInsets.all(20),
+        //         child: const Text("Item 2",
+        //           style: TextStyle(color: Colors.black),), 
+        //       ),
+        //       Container(
+        //         height: 100,
+        //         width: 100,
+        //         decoration: BoxDecoration(
+        //           color: const Color.fromARGB(255, 255, 178, 63),
+        //           borderRadius: BorderRadius.circular(20), // ขอบมน
+        //         ),
+        //         padding: const EdgeInsets.all(20),
+        //         child: const Text("Item 3",
+        //           style: TextStyle(color: Colors.black),), 
+        //       ),   
+        //   ]),
+        // ),
 
         floatingActionButton: FloatingActionButton(
             onPressed: () {
               print("Hello");
             },
-            child: const Icon(Icons.contact_phone),
-          )
+            child: const Icon(Icons.filter_drama_outlined),
+        )
         
       ),
         
